9c5d89aea28536939ff5536090221a1b894226db
Camille Zajac <19757948+camillezajac@users.noreply.github.com>
2021-01-05 13:37:04 -0800

    Update pearsonCorrelation.py

--- pearsonCorrelation.py
+++ pearsonCorrelation.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 import scipy.stats as stats
 import datetime
 
@@ -7,6 +8,12 @@ end = datetime.datetime(2020,1,1)
 
 VIX = yf.download("^VIX", start, end, output_format='pandas')
 SPY = yf.download("^GSPC", start, end, output_format='pandas')
+plt.figure(figsize=(10, 4))
+plt.plot(SPY.index, SPY['Adj Close'])
+plt.plot(VIX.index, VIX['Adj Close'])
+plt.legend(['SPY','VIX'],loc=2)
+plt.title('Times Series');
+plt.show()
 
 c, p = stats.pearsonr(VIX['Adj Close'], SPY['Adj Close'])
 print(f"VIX vs SPY Pearson Correlation: {c}\n")
